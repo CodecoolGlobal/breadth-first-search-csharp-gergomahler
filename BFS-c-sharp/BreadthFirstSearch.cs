d01c3f2f3797d28ee850c6c56825229397d27c37
gergomahler <49788003+gergomahler@users.noreply.github.com>
2020-06-24 11:05:14 +0200

    Distance and FriendsOfFriends Done

---
+++ BFS-c-sharp/BreadthFirstSearch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using BFS_c_sharp.Model;
+
+namespace BFS_c_sharp
+{
+    class BreadthFirstSearch
+    {
+        private readonly List<UserNode> _userGraph;
+
+        public BreadthFirstSearch(List<UserNode> userGraph)
+        {
+            _userGraph = userGraph;
+        }
+
+        public int GetDistance(UserNode userFrom, UserNode userTo)
+        {
+            Queue<KeyValuePair<UserNode, int>> usersToVisit = new Queue<KeyValuePair<UserNode, int>>();
+            List<UserNode> visited = new List<UserNode>();
+
+            usersToVisit.Enqueue(new KeyValuePair<UserNode, int>(userFrom, 0));
+
+            while (usersToVisit.Count != 0)
+            {
+                KeyValuePair<UserNode, int> user = usersToVisit.Dequeue();
+                visited.Add(user.Key);
+                foreach (UserNode friend in user.Key.Friends)
+                {
+                    if (friend.Id.Equals(userTo.Id))
+                    {
+                        return user.Value + 1;
+                    }
+                    usersToVisit.Enqueue(new KeyValuePair<UserNode, int>(friend, user.Value + 1));
+                }
+            }
+            return 0;
+        }
+
+        public List<UserNode> GetFriendsOfFriends(UserNode user, int distance)
+        {
+            List<UserNode> visited = new List<UserNode>();
+            Queue<KeyValuePair<UserNode, int>> usersToVisit = new Queue<KeyValuePair<UserNode, int>>();
+
+            usersToVisit.Enqueue(new KeyValuePair<UserNode, int>(user, 0));
+
+            while (usersToVisit.Count != 0)
+            {
+                KeyValuePair<UserNode, int> currentUser = usersToVisit.Dequeue();
+
+                if (currentUser.Value > distance)
+                {
+                    break;
+                }
+
+                visited.Add(currentUser.Key);
+
+                foreach (UserNode friend in currentUser.Key.Friends)
+                {
+                    if (visited.Contains(friend) || usersToVisit.Select(u => u.Key).Contains(friend)) continue;
+                    usersToVisit.Enqueue(new KeyValuePair<UserNode, int>(friend, currentUser.Value + 1));
+                }
+            }
+            return visited.Where(u => !u.Equals(user)).ToList();
+        }
+    }
+}
--- BFS-c-sharp/Program.cs
+++ BFS-c-sharp/Program.cs
@@ -10,11 +10,25 @@ namespace BFS_c_sharp
         {
             RandomDataGenerator generator = new RandomDataGenerator();
             List<UserNode> users = generator.Generate();
+            BreadthFirstSearch bfs = new BreadthFirstSearch(users);
 
             foreach (var user in users)
             {
                 Console.WriteLine(user);
             }
+
+            Random random = new Random();
+
+            UserNode user1 = users[random.Next(0, users.Count)];
+            UserNode user2 = users[random.Next(0, users.Count)];
+
+            Console.WriteLine($"Distance between {user1} and {user2} is: {bfs.GetDistance(user1, user2)}");
+
+            Console.WriteLine($"Friends of {user1} with distance 2 are:");
+            foreach (var friend in bfs.GetFriendsOfFriends(user1, 2)) {
+                Console.WriteLine(friend);
+            }
+
 
             Console.WriteLine("Done");
             Console.ReadKey();
